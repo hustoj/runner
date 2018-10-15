200cb8466f5b6e5d183642c991b2b6bfc89a7d64
He Jun <freefcw@gmail.com>
2018-10-16 01:03:37 +0800

    refactor(task): refactor code

--- src/exec.go
+++ src/exec.go
@@ -13,66 +13,97 @@ type RunningTask struct {
 	Status      uint64
 	Exit        bool
 	prevRax     uint64
-	pid			int
-	Stop		bool
+	pid         int
+	status syscall.WaitStatus
+	rusage syscall.Rusage
 }
 
 func (task *RunningTask) Run() {
-	var status syscall.WaitStatus
-	var rusage syscall.Rusage
-	var err error
-
-	task.Exit = true
-	task.prevRax = 0
-
 	task.pid = runProcess()
 	logrus.Debugf("child pid is %d", task.pid)
 
-	for {
-		var pid1 int
-		pid1, err = syscall.Wait4(task.pid, &status, 0, &rusage)
-		if pid1 == 0 {
-			logrus.Debugf("not found process")
-			break
-		}
-		checkPanic(err)
+	task.trace()
+}
+func (task *RunningTask) trace() {
+	task.Exit = true
+	task.prevRax = 0
 
-		if status.Exited() {
-			logrus.Infof("Exited: %#v\n", rusage)
+	for {
+		failed := task.wait()
+		if failed {
 			break
 		}
-		if status.StopSignal() != syscall.SIGTRAP {
-			logrus.Infoln("Signal: ", status.StopSignal())
-			if status.StopSignal() != syscall.SIGTERM {
-				logrus.Infof("get signal, send SIGTERM")
-				syscall.Kill(task.pid, syscall.SIGTERM)
-			} else {
-				logrus.Debugf("\n%#v\n", rusage)
-				syscall.Kill(task.pid, syscall.SIGKILL)
+
+		if task.Exited() {
+			break
+		}
+		if !task.trapped() {
+			logrus.Infoln("Signal by: ", task.status.StopSignal())
+
+			if task.gotTerminate() {
+				logrus.Debugf("\n%#v\n", task.rusage)
+				task.kill(syscall.SIGKILL)
 				break
+			} else {
+				logrus.Infof("get signal, send SIGTERM")
+				task.kill(syscall.SIGTERM)
 			}
 			continue
 		}
-		task.DetectRegs()
-		if task.Stop {
+
+		if task.detectRegs() {
 			break
 		}
+
 		task.Exit = !task.Exit
 
-		err = syscall.PtraceSyscall(task.pid, 0)
-		if err != nil {
-			logrus.Infof("err is %v", err)
-		}
+		task.Continue()
 	}
 }
 
-func (task *RunningTask) DetectRegs() {
+func (task *RunningTask) wait() bool {
+	pid1, err := syscall.Wait4(-1, &task.status, 0, &task.rusage)
+	if pid1 == 0 {
+		logrus.Debugf("not found process")
+		return true
+	}
+	checkPanic(err)
+	return false
+}
+
+func (task *RunningTask) Continue() {
+	err := syscall.PtraceSyscall(task.pid, 0)
+	if err != nil {
+		logrus.Infof("err is %v", err)
+	}
+}
+
+func (task *RunningTask) kill(signal syscall.Signal) {
+	syscall.Kill(task.pid, signal)
+}
+
+func (task *RunningTask) gotTerminate() bool {
+	return task.status.StopSignal() == syscall.SIGTERM
+}
+
+func (task *RunningTask) Exited() bool {
+	if task.status.Exited() {
+		logrus.Infof("Exited: %#v\n", task.rusage)
+		return true
+	}
+	return false
+}
+
+func (task *RunningTask) trapped() bool {
+	return task.status.StopSignal() == syscall.SIGTRAP
+}
+
+func (task *RunningTask) detectRegs() bool {
 	var regs syscall.PtraceRegs
 	err := syscall.PtraceGetRegs(task.pid, &regs)
 	if task.Exit {
 		if err != nil {
-			task.Stop = true
-			return
+			return true
 		}
 		if task.prevRax != regs.Orig_rax {
 			logrus.Infof(">> %16v", getName(regs.Orig_rax))
@@ -85,4 +116,5 @@ func (task *RunningTask) DetectRegs() {
 	if task.prevRax == syscall.SYS_EXIT_GROUP {
 		logrus.Infof("SYS_EXIT_GROUP")
 	}
+	return false
 }

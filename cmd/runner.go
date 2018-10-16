7d9afb64d8f5ce90a418f40148ddf8f4ed0537bc
He Jun <freefcw@gmail.com>
2018-10-17 00:08:21 +0800

    feat(log): set default log level is info

--- cmd/runner.go
+++ cmd/runner.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
 	"hustoj/runner/src"
 )
 
 func main() {
+	logrus.SetLevel(logrus.InfoLevel)
 	task := runner.RunningTask{}
 	task.Run()
 }

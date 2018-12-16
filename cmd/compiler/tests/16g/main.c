56ae0938666163748bde57ffa2ce82fe7c5b117f
He Jun <freefcw@gmail.com>
2018-12-17 00:26:58 +0800

    tests(compile): add bombos

---
+++ cmd/compiler/tests/16g/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int m[-1u]={1};
+
+int main()
+{
+	printf("hello world!");
+	return 0;
+}
---
+++ cmd/compiler/tests/bigexe/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define  A 1+
+#define  B A A A A A A A A A A A A A A A A A A A A  1
+#define  C B B B B B B B B B B B B B B B B B B B B  1
+#define  D C C C C C C C C C C C C C C C C C C C C  1
+#define  E D D D D D D D D D D D D D D D D D D D D  1
+#define  F E E E E E E E E E E E E E E E E E E E E  1
+#define  G F F F F F F F F F F F F F F F F F F F F  1
+#define  H G G G G G G G G G G G G G G G G G G G G  1
+#define  I H H H H H H H H H H H H H H H H H H H H  1
+#define  J I I I I I I I I I I I I I I I I I I I I  1
+#define  K J J J J J J J J J J J J J J J J J J J J  1
+#define  L K K K K K K K K K K K K K K K K K K K K  1
+#define  M L L L L L L L L L L L L L L L L L L L L  1
+
+
+int main(){
+    printf("%d\n", M);
+    return 0;
+}
